48ede0007debc6606185aa9d3092baa9fc3200e5
Edwin Kwame Amoakwa <edwin.amoakwa@gmail.com>
2024-11-07 16:57:50 +0000

    updates

---
+++ src/main/java/com/statelyhub/elections/Constant.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.statelyhub.elections;
+
+/**
+ *
+ * @author edwin
+ */
+public class Constant {
+    public static final String SENDER_OD = "MyAstra";
+}
